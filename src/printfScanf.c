32d5b81b5e7cd3b97e903c330abfa90bdb01df16
IlIllIllIlIlllII <ilillillililllii@0910@gmail.com>
2025-09-01 14:43:35 +0900

    feat: study_C (printfScanf)

---
+++ src/printfScanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main_printfscanf(void) {
+    // int input;
+    // scanf_s("%d", &input);
+    // printf("입력값 : %d\n", input);
+
+    // int one, two, three;
+    // printf("3개의 정수를 입력하세요 :");
+    // scanf_s("%d %d %d", &one, &two, &three);
+    // printf("첫번째 값 %d\n", one);
+    // printf("두번째 값 %d\n", two);
+    // printf("세번째 값 %d\n", three);
+
+    float f = 46.4f;
+    printf("%.1f\n", f);
+
+    return 0;
+}
