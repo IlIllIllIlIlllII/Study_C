0b65daa1659f3c30be57e26eb830efdc2c1965a5
IlIllIllIlIlllII <ilillillililllii0910@gmail.com>
2025-09-30 16:27:37 +0900

    feat: 아스키코드

--- src/string.c
+++ src/string.c
@@ -31,7 +31,7 @@ int main() {
     // char c_array[6] = { 'c', 'o', 'd', 'i', 'n', 'g' };
     // printf("%s\n", c_array);
 
-    char c_array[10] = { 'c', 'o', 'd', 'i', 'n', 'g' };
+    // char c_array[10] = { 'c', 'o', 'd', 'i', 'n', 'g' };
     // printf("%s\n", c_array);
     // for (int i = 0; i < sizeof(c_array); i++) {
     //     printf("%c\n", c_array[i]);
@@ -40,11 +40,39 @@ int main() {
     //     printf("%d\n", c_array[i]); // ASCII 코드 값 (NULL 문자는 0 으로 출력됨.)
     // }
 
-    // 문자열 입력받기 : 경찰 조서 쓰기 예제
-    char name[256];
-    printf("이름을 입력하세요: ");
-    scanf("%s", name, sizeof(name));
-    printf("%s\n", name);
+    // // 문자열 입력받기 : 경찰 조서 쓰기 예제
+    // char name[256];
+    // printf("이름을 입력하세요: ");
+    // scanf("%s", name, sizeof(name));
+    // printf("%s\n", name);
 
+    // 참고 : ASCII 코드란 ANSI (미국표준협회) 에서 제시한 표준 코드 체게
+    // 7bit , 128 개 코드 (0~127)
+    // a : 97 (문자 a 의 아스키코드 정수값)
+    // A : 65
+    // 0 : 48
+
+    // printf("%c\n", 'a');
+    // printf("%d\n", 'a');
+    //
+    // printf("%c\n", 'b');
+    // printf("%d\n", 'b');
+    //
+    // printf("%c\n", 'A');
+    // printf("%d\n", 'A');
+    //
+    // printf("%c\n", '\0');
+    // printf("%d\n", '\0');
+    //
+    // printf("%c\n", '0');
+    // printf("%d\n", '0');
+    //
+    // printf("%c\n", '1');
+    // printf("%d\n", '1');
+
+    // 참고2 : 0~127 사이의 아스키코드 정수값에 해당하는 문자 확인
+    for (int i = 0; i <= 127; i++) {
+        printf("아스키코드 정수 %d : %c\n", i ,i);
+    }
     return 0;
 }
